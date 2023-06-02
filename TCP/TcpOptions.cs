2d9ff30a0dd732db906a66b049fa57c6b02026b6
Chad Cooper <chadcooper@arcta.co.uk>
2023-06-02 20:12:01 +0100

    Add TCP option for type; streaming, client

--- TCP/TcpOptions.cs
+++ TCP/TcpOptions.cs
@@ -4,4 +4,10 @@ public class TcpOptions
     public const string Tcp = "Tcp";
     public required string IPAddress { get; set; }
     public required int Port { get; set; }
+    public TcpMode TcpMode { get; set; } = TcpMode.Server;
+}
+public enum TcpMode
+{
+    Server = 0,
+    Client = 1
 }
